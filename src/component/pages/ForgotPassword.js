b64e84374381b01d4fdcaabbdd03aa24040f34ed
Mahamadou <mmohmo@yahoo.com>
2023-12-01 20:31:40 -0500

    forgot password page added

--- src/component/pages/ForgotPassword.js
+++ src/component/pages/ForgotPassword.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { CustomInput } from '../CustomInput'
 
 export const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
+    <div className="py-5" style={{backgroundColor: '#ffd333', minHeight:"100vh"}}>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
+        <h3 className="text-center">Forgot Password</h3>
+        <p className="text-center">Please enter your registered email to get a reset link.</p>
+        <form action="">
+          <CustomInput type="text" label="Email address" id="email"/>
+          <button className="border-0 px-3 py-2 text-white fw-bold w-100" type="submit" style={{backgroundColor: '#ffd333'}}>Send Link</button>
+        </form>
+      </div>
+    </div>
   )
 }
--- src/component/pages/Login.js
+++ src/component/pages/Login.js
@@ -12,8 +12,8 @@ export const Login = () => {
         <h3 className="text-center">Login</h3>
         <p className="text-center">Log in to your account to continue.</p>
         <form action="">
-          <CustomInput type="text" placeholder="Email address" id="email"/>
-          <CustomInput type="password" placeholder="Password" id="pass"/>
+          <CustomInput type="text" label="Email address" id="email"/>
+          <CustomInput type="password" label="Password" id="pass"/>
           <button className="border-0 px-3 py-2 text-white fw-bold w-100" type="submit" style={{backgroundColor: '#ffd333'}}>Log In</button>
         </form>
       </div>
