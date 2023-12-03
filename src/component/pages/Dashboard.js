a94d077d5f57781af9773ac82350fa2da7199479
Mahamadou <mmohmo@yahoo.com>
2023-12-02 22:57:27 -0500

    dashboard card added

--- src/component/pages/Dashboard.js
+++ src/component/pages/Dashboard.js
@@ -1,14 +1,64 @@
 import React from 'react'
+import { HiOutlineDotsVertical } from "react-icons/hi";
 
 export const Dashboard = () => {
   return (
-    <div>
+    <div className="bg-light">
         <h3 className="mb-4">Dashboard</h3>
         <div>
             <div></div>
             <div></div>
             <div></div>
             <div></div>
+        </div>
+        <div className="container">
+            <div className="row">
+                <div className=" col-md-4">
+                    <div className="card">
+                        <div className="d-flex justify-content-between">
+                            <h4>Top sells</h4>
+                            <HiOutlineDotsVertical className="fs-4"/>
+                        </div>
+                        <div>
+                            <h4>$ 3799.00</h4>
+                            <div className="d-flex justify-content-between">
+                                <p>34.7%</p>
+                                <p>Compare to April 2021</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className=" gap-3 col-md-4">
+                    <div className="card">
+                        <div className="d-flex justify-content-between">
+                            <h4>Top sells</h4>
+                            <HiOutlineDotsVertical className="fs-4"/>
+                        </div>
+                        <div>
+                            <h4>$ 3799.00</h4>
+                            <div className="d-flex justify-content-between">
+                                <p>34.7%</p>
+                                <p>Compare to April 2021</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className=" col-md-4">
+                    <div className="card">
+                        <div className="d-flex justify-content-between">
+                            <h4>Top sells</h4>
+                            <HiOutlineDotsVertical className="fs-4"/>
+                        </div>
+                        <div>
+                            <h4>$ 3799.00</h4>
+                            <div className="d-flex justify-content-between">
+                                <p>34.7%</p>
+                                <p>Compare to April 2021</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
