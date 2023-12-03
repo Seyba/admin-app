977545d20a6fd14baed00b38979e367b3854f7e0
Mahamadou <mmohmo@yahoo.com>
2023-12-02 21:04:57 -0500

    Logo added

--- src/component/MainLayout.js
+++ src/component/MainLayout.js
@@ -7,9 +7,9 @@ import {
   VideoCameraOutlined,
 } from '@ant-design/icons';
 import { Layout, Menu, Button, theme } from 'antd';
-import { AiOutlineDashboard } from 'react-icons/ai'
+import { AiOutlineDashboard, AiOutlinePicLeft } from 'react-icons/ai'
 import { MdOutlineShoppingCart } from "react-icons/md";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link, Outlet } from 'react-router-dom'
 import { FaClipboardList, FaRegUser ,FaBloggerB} from "react-icons/fa";
 
 
@@ -28,133 +28,146 @@ export const MainLayout= () => {
     return(
         <Layout>
             <Sider trigger={null} collapsible collapsed={collapsed}>
-            <div className="demo-logo-vertical" />
-            <Menu
-                theme="dark"
-                mode="inline"
-                defaultSelectedKeys={['']}
-                onClick={({ key }) => {
-                    if(key === 'signout'){
+                <div className="logo">
+                    <h2 className="text-white fs-5 text-center py-3 mb-0 ">DevShop</h2>
+                </div>
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['']}
+                    onClick={({ key }) => {
+                        if(key === 'signout'){
 
-                    } else {
-                        navigate(key)
-                    }
-                }}  
-                items={[
-                    {
-                        key: 'Dashboard',
-                        icon: <AiOutlineDashboard className="fs-4"/>,
-                        label: 'Dashboard',
-                    },
-                    {
-                        key: 'Catalog',
-                        icon: <MdOutlineShoppingCart className="fs-4 "/>,
-                        label: 'Catalog',
-                        children: [
-                            {
-                                key: 'Product',
-                                label:'Add Product'
-                            },
-                            {
-                                key: 'Product-list',
-                                label:'Product List'
-                            },
-                            {
-                                key: 'Category',
-                                label:'Brand'
-                            },
-                            {
-                                key:'list-category',
-                                label:'Brand List'
-                            },
-                            {
-                                key:'Category',
-                                label:'Category'
-                            },
-                            {
-                                key:'list-category',
-                                label:'Category List'
-                            },
-                            {
-                                key:'Color',
-                                label:'Color'
-                            },
-                            {
-                                key:'list-color',
-                                label:'Color List'
-                            }
-                            
-                        ]
-                    },
-                    {
-                        key: 'Customers',
-                        icon: <FaRegUser className="fs-4"/>,
-                        label: 'Customers',
-                    },
-                    {
-                        key: 'Orders',
-                        icon: <FaClipboardList className="fs-4"/>,
-                        label: 'Orders',
-                    },
-                    {
-                        key: 'Blog',
-                        icon: <FaBloggerB className="fs-4"/>,
-                        label: 'Blogs',
-                        children:[
-                            {
-                                key: 'Blog',
-                                label: 'Add Blog'
-                            },
-                            {
-                                key: 'list-blog',
-                                label: 'Blog List'
-                            },
-                            {
-                                key: 'blog-category',
-                                label: 'Add Blog Category'
-                            },
-                            {
-                                Key: 'blog-category-list',
-                                label: 'Blog Category List'
-                            }
-                        ]
-                    },
-                    {
-                        key: 'Enquiries',
-                        icon: <AiOutlineDashboard className="fs-4"/>,
-                        label: 'Enquiries',
-                    }
-                ]}
-            />
+                        } else {
+                            navigate(key)
+                        }
+                    }}  
+                    items={[
+                        {
+                            key: 'Dashboard',
+                            icon: <AiOutlineDashboard className="fs-4"/>,
+                            label: 'Dashboard',
+                        },
+                        {
+                            key: 'Catalog',
+                            icon: <MdOutlineShoppingCart className="fs-4 "/>,
+                            label: 'Catalog',
+                            children: [
+                                {
+                                    key: 'Product',
+                                    label:'Add Product'
+                                },
+                                {
+                                    key: 'Product-list',
+                                    label:'Product List'
+                                },
+                                {
+                                    key: 'Category',
+                                    label:'Brand'
+                                },
+                                {
+                                    key:'list-category',
+                                    label:'Brand List'
+                                },
+                                {
+                                    key:'Category',
+                                    label:'Category'
+                                },
+                                {
+                                    key:'list-category',
+                                    label:'Category List'
+                                },
+                                {
+                                    key:'Color',
+                                    label:'Color'
+                                },
+                                {
+                                    key:'list-color',
+                                    label:'Color List'
+                                }
+                                
+                            ]
+                        },
+                        {
+                            key: 'Customers',
+                            icon: <FaRegUser className="fs-4"/>,
+                            label: 'Customers',
+                        },
+                        {
+                            key: 'Orders',
+                            icon: <FaClipboardList className="fs-4"/>,
+                            label: 'Orders',
+                        },
+                        {
+                            key: 'Blog',
+                            icon: <FaBloggerB className="fs-4"/>,
+                            label: 'Blogs',
+                            children:[
+                                {
+                                    key: 'Blog',
+                                    label: 'Add Blog'
+                                },
+                                {
+                                    key: 'list-blog',
+                                    label: 'Blog List'
+                                },
+                                {
+                                    key: 'blog-category',
+                                    label: 'Add Blog Category'
+                                },
+                                {
+                                    Key: 'blog-category-list',
+                                    label: 'Blog Category List'
+                                }
+                            ]
+                        },
+                        {
+                            key: 'Enquiries',
+                            icon: <FaClipboardList className="fs-4"/>,
+                            label: 'Enquiries',
+                        }
+                    ]}
+                />
             </Sider>
             <Layout>
-            <Header
-                style={{
-                padding: 0,
-                background: colorBgContainer,
-                }}
-            >
-                <Button
-                type="text"
-                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                onClick={() => setCollapsed(!collapsed)}
-                style={{
-                    fontSize: '16px',
-                    width: 64,
-                    height: 64,
-                }}
-                />
-            </Header>
-            <Content
-                style={{
-                margin: '24px 16px',
-                padding: 24,
-                minHeight: 280,
-                background: colorBgContainer,
-                }}
-            >
-                Content
-            </Content>
+                <Header
+                    className="d-flex justify-content-between ps-1 pe-5"
+                    style={{
+                    padding: 0,
+                    background: colorBgContainer,
+                    }}
+                >
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+                    <div className="d-flex gap-3 align-items-center ">
+                        <div></div>
+                        <div>
+                            <div><img src="" alt=""/></div>
+                            <div>
+                                <h5>John Doe</h5>
+                                <p>JohnDoe@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                </Header>
+                <Content
+                    style={{
+                    margin: '24px 16px',
+                    padding: 24,
+                    minHeight: 280,
+                    background: colorBgContainer,
+                    }}
+                >
+                    Content
+                </Content>
             </Layout>
         </Layout>
     )
