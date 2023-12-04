2f68d30ce1ab3d07568719310b8d7b8de96b3e1e
Mahamadou <mmohmo@yahoo.com>
2023-12-03 22:34:22 -0500

    widget and chart section added

--- src/component/pages/Dashboard.js
+++ src/component/pages/Dashboard.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import { CgArrowTopRight } from "react-icons/cg";
-
+import { Link } from 'react-router-dom'
 
 export const Dashboard = () => {
   return (
     <div>
         <h3 className="mb-4">Dashboard</h3>
-        <div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
         <div className="container">
             <div className="row">
                 <div className=" col-md-4">
@@ -23,7 +17,7 @@ export const Dashboard = () => {
                         </div>
                         <div className="d-flex justify-content-between align-items-center">
                             <h4 className="indicator-value">$ 3799.00</h4>
-                            <div className="">
+                            <div>
                                 <p className="mb-0 text-end indicator-delta-value"> <CgArrowTopRight className="mb-1 indicator-delta-value"/> 34.7%</p>
                                 <p>Compare to April 2021</p>
                             </div>
@@ -31,33 +25,62 @@ export const Dashboard = () => {
                     </div>
                 </div>
                 <div className="col-md-4">
-                    <div className="mb-3 card">
+                    <div className="mb-3 p-3 card">
                         <div className="d-flex justify-content-between">
-                            <h4>Top sells</h4>
-                            <HiOutlineDotsVertical className="fs-4"/>
+                            <h4 className="widget-header">Top sells</h4>
+                            <HiOutlineDotsVertical className="widget-link"/>
                         </div>
-                        <div>
-                            <h4>$ 3799.00</h4>
-                            <div className="d-flex justify-content-between">
-                                <p>34.7%</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h4 className="indicator-value">$ 3799.00</h4>
+                            <div>
+                                <p className="mb-0 text-end indicator-delta-value"> <CgArrowTopRight className="mb-1 indicator-delta-value"/> 34.7%</p>
                                 <p>Compare to April 2021</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-4">
-                    <div className="mb-3 card">
+                    <div className="mb-3 p-3 card">
                         <div className="d-flex justify-content-between">
-                            <h4>Top sells</h4>
-                            <HiOutlineDotsVertical className="fs-4"/>
+                            <h4 className="widget-header">Top sells</h4>
+                            <HiOutlineDotsVertical className="widget-link"/>
                         </div>
-                        <div>
-                            <h4>$ 3799.00</h4>
-                            <div className="d-flex justify-content-between">
-                                <p>34.7%</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h4 className="indicator-value">$ 3799.00</h4>
+                            <div>
+                                <p className="mb-0 text-end indicator-delta-value"> <CgArrowTopRight className="mb-1 indicator-delta-value"/> 34.7%</p>
                                 <p>Compare to April 2021</p>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-3">
+                    <div className="card p-3">
+                        <div className="d-flex justify-content-between align-items-center">
+                            <h3>Active users</h3>
+                            <HiOutlineDotsVertical />
+                        </div>
+                        <div className="text-center">
+                            <h3>148</h3>
+                        </div>
+                        <div>
+                            <div>
+                                <h3>Active pages</h3>
+                                <h3>Users</h3>
+                            </div>
+                            <ul>
+                                <li>
+                                    <Link to="">/products/brandix-z4</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-9">
+                    <div className="card">
+
                     </div>
                 </div>
             </div>
