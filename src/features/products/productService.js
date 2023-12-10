3d5130eb49e8a29d806f428d01e5542638eb6ce9
Mahamadou <mmohmo@yahoo.com>
2023-12-09 22:13:27 -0500

    product service added

---
+++ src/features/products/productService.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import { base_url } from '../../utils/base_url'
+
+const getProducts = async () => {
+    const response = await axios.get(`${base_url}products`)
+
+    // if(response.data){
+    //     localStorage.setItem('user', JSON.stringify(response.data))
+    // }
+    
+    return response.data
+}
+
+const productService = { 
+    getProducts
+}
+
+export default productService
