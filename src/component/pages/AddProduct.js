11d829942dd66c458525723b81640f67e449f8f2
Mahamadou <mmohmo@yahoo.com>
2023-12-24 22:30:29 -0500

    close button added

--- src/component/pages/AddProduct.js
+++ src/component/pages/AddProduct.js
@@ -212,7 +212,12 @@ export const AddProduct = () => {
             {
               imgState.map((i, j) => {
                 return(
-                  <div key={j}>
+                  <div className="position-relative" key={j}>
+                    <Link className="position-absolute bg-dark text-white" style={{left: '180px', top: '5px'}}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                      </svg>
+                    </Link>
                     <img src={i.url} alt="product" width={200} height={200}/>
                   </div>
                 )
